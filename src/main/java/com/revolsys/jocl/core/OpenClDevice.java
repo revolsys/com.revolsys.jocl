f868b799481fa19442b2be80b932c3720fb5ad5f
paul <paul@PAUSTIN-WIN10>
2018-12-07 11:17:52 -0800

    JOCL device info

--- src/main/java/com/revolsys/jocl/core/OpenClDevice.java
+++ src/main/java/com/revolsys/jocl/core/OpenClDevice.java
@@ -1,6 +1,11 @@
 package com.revolsys.jocl.core;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 import org.jocl.CL;
+import org.jocl.Pointer;
+import org.jocl.Sizeof;
 import org.jocl.cl_device_id;
 import org.jocl.cl_platform_id;
 
@@ -10,21 +15,75 @@ public class OpenClDevice implements AutoCloseable {
 
   private final OpenClPlatform platform;
 
-  private final cl_device_id id;
+  private final cl_device_id deviceId;
 
   public OpenClDevice(final OpenClPlatform platform, final cl_device_id id) {
     this.platform = platform;
     this.platformId = platform.getId();
-    this.id = id;
+    this.deviceId = id;
   }
 
   @Override
   public void close() {
-    CL.clReleaseDevice(this.id);
+    CL.clReleaseDevice(this.deviceId);
   }
 
   public cl_device_id getId() {
-    return this.id;
+    return this.deviceId;
+  }
+
+  public int getInfoInt(final int paramName) {
+    return getInfoInts(paramName, 1)[0];
+  }
+
+  public int[] getInfoInts(final int paramName, final int numValues) {
+    final int values[] = new int[numValues];
+    CL.clGetDeviceInfo(this.deviceId, paramName, Sizeof.cl_int * numValues, Pointer.to(values),
+      null);
+    return values;
+  }
+
+  public long getInfoLong(final int paramName) {
+    return getInfoLongs(paramName, 1)[0];
+  }
+
+  public long[] getInfoLongs(final int paramName, final int numValues) {
+    final long values[] = new long[numValues];
+    CL.clGetDeviceInfo(this.deviceId, paramName, Sizeof.cl_long * numValues, Pointer.to(values),
+      null);
+    return values;
+  }
+
+  public long getInfoSize(final int paramName) {
+    return getInfoSizes(paramName, 1)[0];
+  }
+
+  public long[] getInfoSizes(final int paramName, final int numValues) {
+    final ByteBuffer buffer = ByteBuffer.allocate(numValues * Sizeof.size_t)
+      .order(ByteOrder.nativeOrder());
+    CL.clGetDeviceInfo(this.deviceId, paramName, Sizeof.size_t * numValues, Pointer.to(buffer),
+      null);
+    final long values[] = new long[numValues];
+    if (Sizeof.size_t == 4) {
+      for (int i = 0; i < numValues; i++) {
+        values[i] = buffer.getInt(i * Sizeof.size_t);
+      }
+    } else {
+      for (int i = 0; i < numValues; i++) {
+        values[i] = buffer.getLong(i * Sizeof.size_t);
+      }
+    }
+    return values;
+  }
+
+  public String getInfoString(final int paramName) {
+    final long size[] = new long[1];
+    CL.clGetDeviceInfo(this.deviceId, paramName, 0, null, size);
+
+    final byte buffer[] = new byte[(int)size[0]];
+    CL.clGetDeviceInfo(this.deviceId, paramName, buffer.length, Pointer.to(buffer), null);
+
+    return new String(buffer, 0, buffer.length - 1);
   }
 
   public OpenClPlatform getPlatform() {
@@ -41,6 +100,6 @@ public class OpenClDevice implements AutoCloseable {
 
   @Override
   public String toString() {
-    return this.id.toString();
+    return this.deviceId.toString();
   }
 }
